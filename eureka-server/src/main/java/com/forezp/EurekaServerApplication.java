2034776a364ea63ab7e59a50e7acf070ac7bcb7f
xiaolong9527 <2442241600@qq.com>
2018-01-09 17:40:02 +0800

    试试

---
+++ eureka-server/src/main/java/com/forezp/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package com.forezp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 
+ * @author zhou
+ * 启动一个服务注册中心，只需要一个注解@EnableEurekaServer，
+ * 这个注解需要在springboot工程的启动application类上加
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
